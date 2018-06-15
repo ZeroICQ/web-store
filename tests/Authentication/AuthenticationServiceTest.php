9886d0dc2b6e8f55f7474e7899bbaedb972e2dc7
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-06-15 20:11:54 +1000

    Update tests

--- tests/Authentication/AuthenticationServiceTest.php
+++ tests/Authentication/AuthenticationServiceTest.php
@@ -40,10 +40,12 @@ class AuthenticationServiceTest extends TestCase
         $repo = $this->createMock(UserRepository::class);
         $repo->method('findByLogin')->willReturn(
             new User(1, 'username', $cryptedPassword));
-        $repo->expects($this->exactly(2))
+
+        $repo->expects($this->once())
             ->method('findByLogin')->with($this->equalTo('username'));
 
         $auth = new AuthenticationService($repo, 'iopdasojijcoajscx,mzmc,z.xmizqje');
+
         $credentials = $auth->generateCredentials('username', 'wrongpassword');
         $userToken = $auth->authenticate($credentials);
 
@@ -51,17 +53,34 @@ class AuthenticationServiceTest extends TestCase
         $this->assertNull($userToken->getUser());
     }
 
-    public function testRegisterUser()
+    public function testRegisterUserSuccess()
     {
         $repo = $this->createMock(UserRepository::class);
 
         $repo->method('save')->willReturn(1);
         $repo->expects($this->once())
-            ->method('save')->with($this->equalTo(1));
+            ->method('save');
 
         $auth = new AuthenticationService($repo, 'iopdasojijcoajscx,mzmc,z.xmizqje');
-        $auth->registerUser('username', 'password');
+        $userToken = $auth->registerUser('username', 'password');
 
+        $this->assertFalse($userToken->isAnonymous());
+        $this->assertSame('username', $userToken->getUser()->getLogin());
+
+    }
+
+    public function testRegisterUserFail()
+    {
+        $repo = $this->createMock(UserRepository::class);
+
+        $repo->method('save')->willReturn(0);
+        $repo->expects($this->once())
+            ->method('save');
+
+        $auth = new AuthenticationService($repo, 'iopdasojijcoajscx,mzmc,z.xmizqje');
+        $userToken = $auth->registerUser('username', 'password');
+
+        $this->assertTrue($userToken->isAnonymous());
     }
 
 }
