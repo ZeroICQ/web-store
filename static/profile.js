0e3c52922863cf2843aed441bf4f3fedbfbdc18a
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-06-22 21:57:25 +0000

    Save

--- static/profile.js
+++ static/profile.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-    alert(1);
+
+    $(document).on('click', '#save-profile-btn', function (e) {
+        e.preventDefault();
+
+        var login      = $('#login-input').val();
+        var firstName  = $('#firstname-input').val();
+        var secondName = $('#secondname-input').val();
+        var workplace  = $('#workplace-input').val();
+        var bio        = $('#bio-input').val();
+        
+    });
+
+    $(document).on('submit', '#save-profile-form', function (e) {
+        e.preventDefault();
+    });
+
 });
